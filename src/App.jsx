9376c89fa570e653c61b72c8379388054e19961b
MDMS29 <mdmazo30@misena.edu.co>
2024-04-19 22:48:41 -0500

    fix(repo) : Fix files

--- src/App.jsx
+++ src/App.jsx
@@ -7,9 +7,9 @@ import { Proyect } from './components/Proyect.jsx'
 import { Hability } from './components/Hability.jsx'
 import { UseFetch } from './components/UseFetch.jsx'
 
-function App () {
+function App() {
   const data = UseFetch('../src/data/data.json')
-  const { newUserHability, restCountry, ecommerceReact, sectionDropdown } = data
+  const { courses, projects, newUserHability, contacts } = data
 
   return (
     <>
@@ -19,43 +19,50 @@ function App () {
 
       <main>
         <section id='sobremi' className='containerElements'>
-          <Hero />
-          <section className='containerEducation'>
-            <section id='educacion' className='education'>
-              <Titles logo='school' title='Educacion' />
-              <Curses title='Alura latam' name='DESARROLLO PERSONAL G4 - ONE' date='07-12-22' />
-              <Curses title='Alura latam' name='Formación Principiante en Programación G4 - ONE' date=' 03-01-23' />
-              <Curses title='Alura latam' name='HTML5 Y CSS3 PARTE 4: AVANZANDO EN CSS' date='26-12-22' />
-              <Curses title='Alura latam' name='GIT Y GITHUB: CONTROLE Y COMPARTA SU CÓDIGO' date='14-04-23' />
-              <Curses title='Udemy' name='React JS Desde Cero' date='14-04-23' />
+          <Hero contacts={contacts} />
+
+
+          <section id='habilidades' className='containerHability'>
+            <section className='habilitys'>
+              <Titles logo='hability' title='Habilidades' />
+              <section className='contentHabilitys'>
+                {
+                  newUserHability && newUserHability.map(item => {
+                    return (
+                      <Hability key={item.name} name={item.name} logo={item.logo} />
+                    )
+                  })
+                }
+              </section>
             </section>
           </section>
 
-          <section id='proyectos' className='proyects'>
-            <Titles logo='code' title='Proyectos' />
-            <Proyect restCountry={restCountry} />
-            <Proyect restCountry={ecommerceReact} />
-            <Proyect restCountry={sectionDropdown} />
-          </section>
-        </section>
-      </main>
-
-      <footer>
-        <section id='habilidades' className='containerHability'>
-          <section className='habilitys'>
-            <Titles logo='hability' title='Habilidades' />
-            <section className='contentHabilitys'>
+          <section className='containerEducation'>
+            <section id='educacion' className='education'>
+              <Titles logo='school' title='Educacion' />
               {
-                newUserHability && newUserHability.map(item => {
+                courses && courses.map(item => {
                   return (
-                    <Hability key={item.name} name={item.name} logo={item.logo} />
+                    <Curses key={item.name} title={item.title} name={item.name} date={item.date} />
                   )
                 })
               }
             </section>
           </section>
+
+          <section id='proyectos' className='proyects'>
+            <Titles logo='code' title='Proyectos' />
+
+            {
+              projects && projects.map(item => {
+                return (
+                  <Proyect key={item.title} project={item} />
+                )
+              })
+            }
+          </section>
         </section>
-      </footer>
+      </main>
     </>
   )
 }
--- src/components/LinkLogo.jsx
+++ src/components/LinkLogo.jsx
@@ -3,9 +3,9 @@ import style from '../style/linkLogo.module.css'
 
 export const LinkLogo = ({ url, name, logo }) => {
   return (
-    <a href={url} className={style.link}>
+    <a href={url} className={style.link} target='_blank'>
       <div className={style.linkLogo}>
-        <img src={`../src/assets/images/iconWhite/${logo}.svg`} alt='' />
+        <img src={`../src/assets/images/iconWhite/${logo}.svg`} alt={`logo ${logo}`} />
       </div>
       <p>{name}</p>
     </a>
